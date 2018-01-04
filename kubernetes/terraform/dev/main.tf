3b58520e977b886cdd3aec3f7f18dc70fa1fbf1e
Andrej Sedov <sedoy80@gmail.com>
2018-01-04 17:22:59 +0300

    Update kubernetes cluster.

--- kubernetes/terraform/dev/main.tf
+++ kubernetes/terraform/dev/main.tf
@@ -3,11 +3,16 @@ provider "google" {
   region  = "${var.region}"
 }
 
+data "google_container_engine_versions" "reddit_version" {
+  zone = "${var.zone}"
+}
+
 resource "google_container_cluster" "reddit" {
   name = "${var.cluster_name}"
   zone = "${var.zone}"
+  node_version       = "${data.google_container_engine_versions.reddit_version.latest_node_version}"
   initial_node_count = "${var.cluster_node_count}"
-
+  min_master_version = "${data.google_container_engine_versions.reddit_version.latest_master_version}"
   node_config {
     machine_type = "${var.cluster_node_machine_type}"
   }
